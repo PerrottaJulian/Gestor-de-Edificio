835d5b75b751bd692d5770a57b887f4d9694762b
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-04-09 20:43:17 -0300

    Initial commit

---
+++ SmartBuild-Space/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartBuild_Space.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
