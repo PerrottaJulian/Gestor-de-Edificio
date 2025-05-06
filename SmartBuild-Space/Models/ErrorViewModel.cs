0b359c8f89f89dfcf0f3490d4ac6b55cdb3afc3c
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-05-06 11:20:21 -0300

    Reinicio

--- SmartBuild-Space/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartBuild_Space.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
