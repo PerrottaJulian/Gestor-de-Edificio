7c2680b304c844a46350cb074c70b36289cad6a9
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-05-08 12:05:52 -0300

    Funcionamiento Basico
    Falta modelo y base de datos

--- RedBelgrano/RedBelgrano/Controllers/LoginController.cs
+++ RedBelgrano/RedBelgrano/Controllers/LoginController.cs
@@ -1,12 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedBelgrano.Models;
+using System.Timers;
 
 namespace RedBelgrano.Controllers
 {
     public class LoginController : Controller
     {
+
+        UsuarioAdmin helper_usuario = new UsuarioAdmin();
+
+        public LoginController()
+        {
+            helper_usuario.dni = 44563116;
+            helper_usuario.nombre = "Julian Perrotta";
+            helper_usuario.email = "perrotta.julian12@gmail.com";
+            helper_usuario.clave = "admin";
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
+            ViewBag.seeNavbar = false;
             return View();
+        }
+        [HttpPost]
+        public IActionResult Login(UsuarioAdmin admin)
+        {
+
+            if (admin == null )
+            {
+                return Login();
+            }
+
+            if (admin.dni != helper_usuario.dni)
+            {
+                Console.WriteLine("No existe ese Usuario");
+                return Login();
+            }
+            else if (!admin.clave.Equals(helper_usuario.clave))
+            {
+                Console.WriteLine("La contraseña no coincide");
+                return Login();
+            }
+
+            Console.WriteLine("Se logueo correctamente");
+            return RedirectToAction("Index", "Home");
         }
 
     }
--- RedBelgrano/RedBelgrano/Models/Usuario.cs
+++ RedBelgrano/RedBelgrano/Models/Usuario.cs
@@ -1,13 +1,13 @@
-﻿namespace RedBelgrano.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedBelgrano.Models
 {
     public abstract class Usuario
     {
-        public int id { get; set; }
+        [Key]
+        public int dni { get; set; }
         public string nombre { get; set; }
-        public string apellido { get; set; }
-        public int dni {  get; set; }
         public string email { get; set; }
-        public string clave { get; set; }
 
         public void cambiarClave(string NuevaClave)
         {
