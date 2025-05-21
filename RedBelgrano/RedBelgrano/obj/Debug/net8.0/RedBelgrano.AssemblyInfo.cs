86b4bdca44ffeb6109f1ad8d27b18973686a0e93
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-05-21 11:14:07 -0300

    MergeLogueo Correcto

--- RedBelgrano/RedBelgrano/obj/Debug/net8.0/RedBelgrano.AssemblyInfo.cs
+++ RedBelgrano/RedBelgrano/obj/Debug/net8.0/RedBelgrano.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146178d31256671c36ba93b71cd92dedcb85bdad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3410d1710d23bddf3e0d2f703738372ca8ba0c59")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
