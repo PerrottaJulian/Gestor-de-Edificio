12066f9eea1e7d014ec1dfd52336cda812f3a03d
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-06-04 10:09:29 -0300

    Comienzo

---
+++ RedBelgrano/RedBelgrano/Models/Transaccion.cs
@@ -0,0 +1,13 @@
+﻿namespace RedBelgrano.Models
+{
+    public class Transaccion
+    {
+        public int transaccionId { get; set; }
+        public int administradorId { get; set; }
+        public int tipoId { get; set; }
+        public double Monto { get; set; }
+        public string? detalle {  get; set; }
+        public DateTime fecha { get; set; }
+
+    }
+}
