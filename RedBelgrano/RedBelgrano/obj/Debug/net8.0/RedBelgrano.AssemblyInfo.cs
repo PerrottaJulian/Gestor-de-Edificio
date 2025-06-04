e27d00bd08c957526b29c626178308955a409faa
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-06-04 12:25:50 -0300

    Error Funciona

--- RedBelgrano/RedBelgrano/obj/Debug/net8.0/RedBelgrano.AssemblyInfo.cs
+++ RedBelgrano/RedBelgrano/obj/Debug/net8.0/RedBelgrano.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5a6a72866608e3b1710404c4e7662ad0d1fa4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22dcde591fa0db32b03d3a9cd458e6c478a1b63f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
