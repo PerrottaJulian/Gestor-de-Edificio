421a638f15ba4bd1cd517183c42e19956d40105e
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2025-06-17 18:37:26 -0300

    MErge correcto + solucion de errores

--- RedBelgrano/RedBelgrano/obj/Debug/net8.0/RedBelgrano.AssemblyInfo.cs
+++ RedBelgrano/RedBelgrano/obj/Debug/net8.0/RedBelgrano.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b612dd4029ef35d99ff37c38d9cd14199b41d7b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c783be86d0e0d47af54fcb9857c68a145cdd28b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedBelgrano")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
